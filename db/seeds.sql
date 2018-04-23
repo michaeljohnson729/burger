9c8091e2773f303faf220e3ebb3edf4c98f2bb86
Michael Johnson <michaeljohnson@Michaels-MacBook-Pro.local>
2018-04-23 14:14:00 -0700

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Bacon Swiss", 0);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("BBQ", 0);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Turkey", 0);
+
---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
